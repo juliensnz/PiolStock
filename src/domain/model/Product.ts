dd35b4263030f7773f9bb38c62af4dfadfc4a57c
Julien Sanchez <jsanchez@joko.com>
2026-03-22 19:39:43 +0100

    Add format icon

--- src/domain/model/Product.ts
+++ src/domain/model/Product.ts
@@ -1,18 +1,19 @@
-const FORMATS_A2 = 'A2';
-const FORMATS_A3 = 'A3';
-const FORMATS_A4 = 'A4';
-const FORMATS_A5 = 'A5';
-const FORMATS_A6 = 'A6';
-const UNISIZE = 'UNISIZE';
+const FORMAT = {
+  A2: 'A2',
+  A3: 'A3',
+  A4: 'A4',
+  A5: 'A5',
+  A6: 'A6',
+  UNISIZE: 'UNISIZE',
+} as const;
 
-const FORMATS = [FORMATS_A2, FORMATS_A3, FORMATS_A4, FORMATS_A5, FORMATS_A6] as const;
-type Format = typeof FORMATS[number] | typeof UNISIZE;
+type Format = (typeof FORMAT)[keyof typeof FORMAT];
 
 type ProductId = string;
 type Product = {id: ProductId; format: Format; name: string; image: string; stock: number};
 
 const createProducts = (name: string, image: string): Product[] =>
-  FORMATS.map(format => createProduct(name, image, format));
+  Object.values(FORMAT).map(format => createProduct(name, image, format));
 
 const createProduct = (name: string, image: string, format: Format): Product => ({
   id: crypto.randomUUID(),
